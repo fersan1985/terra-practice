94485132f18f9550a8cb366b7301e64612c86418
fersan1985 <72047538+fersan1985@users.noreply.github.com>
2020-11-14 19:01:22 -0300

    Update provider.tf

--- sts-test/provider.tf
+++ sts-test/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
     assume_role {
-        role_arn     = "arn:aws:iam::839056131293:role/fersan1985"
+        role_arn     = "arn:aws:iam::accounthere:role/fersan1985"
         session_name = "fersan1985"
     }
 }
